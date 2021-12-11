1feeccee63770c55958cd3934e47ebdc66dd63a2
Bima Turangga <macbook@macbooks-MacBook-Pro.local>
2021-12-11 19:31:40 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myfirst_flutter/page/landing_pages.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LandingPage());
+  }
+}
